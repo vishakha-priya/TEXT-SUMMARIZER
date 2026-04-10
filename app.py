9f70abf5807f7f4c519004a89ef7d74c6a00317e
vishakha-priya <vishakhapriya234@gmail.com>
2026-04-10 20:02:44 +0530

    added app file

---
+++ app.py
@@ -0,0 +1,93 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+import torch
+import re 
+from fastapi.templating import Jinja2Templates # UI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(title="Text Summarizer App", description="Text Summarization using T5", version="1.0")
+
+model = T5ForConditionalGeneration.from_pretrained(r"E:\backup\New folder\TEXTSUMMARIZERAPP\saved_summary_model\saved_summary_model")
+tokenizer = T5Tokenizer.from_pretrained(r"E:\backup\New folder\TEXTSUMMARIZERAPP\saved_summary_model\saved_summary_model")
+
+
+
+# device
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+model.to(device)
+
+templates = Jinja2Templates(directory=".")
+
+class DialogueInput(BaseModel):
+    dialogue: str
+
+def clean_data(text):
+    text = re.sub(r"\r\n", " ", text) # lines
+    text = re.sub(r"\s+", " ", text) # spaces
+    text = re.sub(r"<.*?>", " ", text) # html tags <p> <h1>
+    text = text.strip().lower()
+    return text
+
+def summarize_dialogue(dialogue : str) -> str:
+    dialogue = clean_data(dialogue) # clean
+    dialogue="summarize:" + dialogue
+
+    # tokenize
+    inputs = tokenizer(
+        dialogue,
+        padding="max_length",
+        max_length=512,
+        truncation=True,
+        return_tensors="pt"
+    ).to(device)
+
+    # generate the summary => token ids
+    model.to(device)
+    targets = model.generate(
+        input_ids=inputs["input_ids"],
+        attention_mask=inputs["attention_mask"],
+        max_length=150,
+        num_beams=4,
+        early_stopping=True
+    )
+    
+    # decoded our output
+    summary = tokenizer.decode(targets[0], skip_special_tokens=True) # EOS, SEP
+    print("summary:",summary)
+    return summary
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# API endpoints
+@app.post("/summarize/")
+async def summarize(dialogue_input: DialogueInput):
+    summary = summarize_dialogue(dialogue_input.dialogue)
+    return {"summary": summary}
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+           
+            
+        }
+      )
